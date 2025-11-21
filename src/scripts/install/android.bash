951c6a8107f5248e2b5801adba9540aea1ea5042
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-11-20 23:06:16 -0600

    Add sudo to group and acl commands in android.bash

--- src/scripts/install/android.bash
+++ src/scripts/install/android.bash
@@ -14,9 +14,18 @@ yay -S --noconfirm \
 
 # Create a group that can write to the android SDK path
 # NOTE: Warnings appear if this isn't done
-groupadd android-sdk
-gpasswd -a "$NEW_USERNAME" android-sdk
-setfacl -R -m g:android-sdk:rwx /opt/android-sdk
-setfacl -d -m g:android-sdk:rwX /opt/android-sdk
+sudo groupadd android-sdk
+sudo gpasswd -a "$NEW_USERNAME" android-sdk
+sudo setfacl -R -m g:android-sdk:rwx /opt/android-sdk
+sudo setfacl -d -m g:android-sdk:rwX /opt/android-sdk
+
+SDK_MANAGER="/opt/android-sdk/cmdline-tools/latest/bin/sdkmanager"
+ANDROID_SYSTEM_IMAGE="$("$SDK_MANAGER" --list | awk '{print $1}' | grep "system-images.*google_apis" | grep -v "CANARY\|ps16k\|playstore\|arm64" | sort | tail -n1)"
+# Install System Image
+echo "y" | "$SDK_MANAGER" --install "$ANDROID_SYSTEM_IMAGE"
+
+AVD_MANAGER="/opt/android-sdk/cmdline-tools/latest/bin/avdmanager"
+# Create the AVD
+echo "n" | "$AVD_MANAGER" create avd --name phone -k "$ANDROID_SYSTEM_IMAGE"
 
 echo "Finished installing Android development tools"
