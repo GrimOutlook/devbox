3640b148d778569bce309ef463295e276978a155
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-11-21 20:06:45 -0600

    Refactor sudoers setup in create_user.bash
    Updated sudoers configuration and added notes on file permissions.

--- src/scripts/configuration/create_user.bash
+++ src/scripts/configuration/create_user.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Set the script to exit when errors are hit
-set -euxo pipefail
+set -euo pipefail
 
 # Install the sudo package so non root users can run commands as root.
 # Also install shadow because for some reason a part of the install is broken if
@@ -10,7 +10,13 @@ pacman -Syyu --needed --noconfirm sudo shadow
 # Add entries to the sudoers file for the install user so it can run all
 # commands without passwords. This is needed because several tools
 # will not allow you to run as root for safety reasons.
-echo "$INSTALL_USER ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
+#
+# NOTE: WHY TF ARE THERE SO MANY HIDDEN RULES ABOUT INCLUDED SUDOER.D FILES?!?!?!
+# For future me, I have learned that to be included the file cannot have a `~` or,
+# the one that got me, a `.` in it and it ALSO MUST end with a newline. The iles 
+# permissions must also be set to 0440
+echo -e "$INSTALL_USER ALL=(ALL) NOPASSWD: ALL\n" >>/etc/sudoers.d/installer
+chmod 0440 /etc/sudoers.d/installer
 
 # Create a user for installation. Variables taken from `env.bash`.
 useradd --comment "Installation user" \
@@ -22,7 +28,8 @@ useradd --comment "Installation user" \
 # Users in the `sudo` group are allowed to run any commands with the sudo
 # command but must enter the correct password.
 groupadd sudo
-echo "%sudo ALL=(ALL:ALL)" >>/etc/sudoers.d/sudo.conf
+echo -e "%sudo ALL=(ALL:ALL) ALL\n" >>/etc/sudoers.d/sudo
+chmod 0440 /etc/sudoers.d/sudo
 # Create the user that will be used for developerment. Variables taken from `input.bash`.
 # TODO: Do I need to add a `groupadd wheel` command to ensure this command
 # doesn't fail?
