a66f285ae9f94a31ec815d59e179c484c1376604
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2026-02-02 08:59:12 -0600

    Add installation of rustup in rust.bash script

--- src/scripts/configuration/rust.bash
+++ src/scripts/configuration/rust.bash
@@ -4,6 +4,11 @@ set -euxo pipefail
 
 echo "Installing Rust components"
 
+yay -S --noconfirm rustup || {
+  echo "Failed to install rustup to devbox"
+  exit 1
+}
+
 rustup toolchain install stable nightly
 
 echo "Finish installing Rust components"
